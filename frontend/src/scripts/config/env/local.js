8cdca2bed67d3f2d8b84e4e9f574495b03edb32a
ciffi <alessio@ciffi.it>
2018-06-07 13:35:26 +0200

    [fe] - test precommit

--- frontend/src/scripts/config/env/local.js
+++ frontend/src/scripts/config/env/local.js
@@ -1,11 +1,11 @@
 const Config = {
-	projectName: 'symfony-template',
-	env: 'local',
-	baseUrl: 'http://symfony-template.local',
-	apiUrl: 'http://symfony-template.local/',
-	shareUrl: 'http://symfony-template.local/',
-	assetsUrl: 'http://symfony-template.local/src/',
-	liveReload: false
+  projectName: 'symfony-template',
+  env: 'local',
+  baseUrl: 'http://symfony-template.local',
+  apiUrl: 'http://symfony-template.local/',
+  shareUrl: 'http://symfony-template.local/',
+  assetsUrl: 'http://symfony-template.local/src/',
+  liveReload: false
 };
 
 export default Config;
